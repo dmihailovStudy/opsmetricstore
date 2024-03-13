28adad6f4634b4b15e914cb83c72b1ae66cbccd4
dm <dmikhailov@technotimes.org>
2024-03-13 17:03:41 +0300

    [INC-6] Added middleware logging

---
+++ internal/logging/responseWritter.go
@@ -0,0 +1,35 @@
+package logging
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
+	"time"
+)
+
+type ResponseWriter struct {
+	gin.ResponseWriter
+	StatusCode    int
+	ContextLength int
+}
+
+func NewResponseWriter(w gin.ResponseWriter) *ResponseWriter {
+	return &ResponseWriter{w, w.Status(), w.Size()}
+}
+
+func (lrw *ResponseWriter) WriteHeader(code int) {
+	lrw.StatusCode = code
+	lrw.ContextLength = lrw.Size()
+	lrw.ResponseWriter.WriteHeader(code)
+}
+
+func (lrw *ResponseWriter) LogQueryParams(c *gin.Context, startTime time.Time) {
+	uri := c.Request.RequestURI
+	method := c.Request.Method
+	log.Info().
+		Str("uri", uri).
+		Str("method", method).
+		Int("status", lrw.StatusCode).
+		Int("contextLength", lrw.ContextLength).
+		Dur("execTime", time.Since(startTime)).
+		Msg("logQueryParams(): stats")
+}
